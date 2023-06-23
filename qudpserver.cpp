87efa2d1f2975af2c0d5b552ebd3c03b10ff05a9
cukier <mauriciocukier@gmail.com>
2023-06-23 07:52:46 -0300

    str comando

--- qudpserver.cpp
+++ qudpserver.cpp
@@ -3,10 +3,36 @@
 
 #include <QUdpSocket>
 
+struct Comando {
+    quint16 id;
+    quint16 cmd;
+    quint16 val;
+};
+
 struct __attribute__((__packed__)) Connection {
     quint32 action;
     quint32 transaction_id;
     quint64 connection_id;
+    Comando comando;
+
+    QString toString() {
+        QString ret;
+
+        ret.append("action ");
+        ret.append(QString("%01").arg(action));
+        ret.append("\nconnection_id ");
+        ret.append(QString("%01").arg(connection_id));
+        ret.append("\ntransaction_id ");
+        ret.append(QString("%01").arg(transaction_id));
+        ret.append("comando\nid");
+        ret.append(QString("%01").arg(comando.id));
+        ret.append("\ncmd");
+        ret.append(QString("%01").arg(comando.cmd));
+        ret.append("\nval");
+        ret.append(QString("%01").arg(comando.val));
+
+        return ret;
+    }
 };
 
 QUdpServer::QUdpServer(QObject *parent)
@@ -30,12 +56,8 @@ QUdpServer::QUdpServer(QObject *parent)
                  << sender.toString()
                  << " porta "
                  << senderPort
-                 << "\naction "
-                 << response.action
-                 << "\nconnection_id "
-                 << response.connection_id
-                 << "\ntransaction_id "
-                 << response.transaction_id;
+                 << '\n'
+                 << response.toString();
         socket->writeDatagram(QByteArray(reinterpret_cast<char*>(&response),
                                          sizeof(response)), sender, senderPort);
 
